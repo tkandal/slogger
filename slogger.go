d84b2a3659b3588030d3661b62da83413a4038c7
Trond Kandal <Trond.Kandal@ntnu.no>
2024-02-10 00:29:41 +0100

    Try to fix bug.

--- slogger.go
+++ slogger.go
@@ -11,6 +11,7 @@ import (
 )
 
 const (
+	badKey     = "!BADKEY"
 	callerSkip = 3
 )
 
@@ -187,9 +188,9 @@ func (sl *SLogger) With(args ...any) *SLogger {
 		return sl
 	}
 	c := sl.clone()
-	c.file.With(args...)
-	c.stdout.With(args...)
-	c.stderr.With(args...)
+	c.file = slog.New(sl.file.Handler().WithAttrs(argsToAttrSlice(args)))
+	c.stdout = slog.New(sl.stdout.Handler().WithAttrs(argsToAttrSlice(args)))
+	c.stderr = slog.New(sl.stderr.Handler().WithAttrs(argsToAttrSlice(args)))
 	return c
 }
 
@@ -198,8 +199,36 @@ func (sl *SLogger) WithGroup(name string) *SLogger {
 		return sl
 	}
 	c := sl.clone()
-	c.file.Handler().WithGroup(name)
-	c.stdout.Handler().WithGroup(name)
-	c.stderr.Handler().WithGroup(name)
+	c.file = slog.New(sl.file.Handler().WithGroup(name))
+	c.stdout = slog.New(sl.stdout.Handler().WithGroup(name))
+	c.stderr = slog.New(sl.stderr.Handler().WithGroup(name))
 	return c
+}
+
+func argsToAttrSlice(args []any) []slog.Attr {
+	var (
+		attr  slog.Attr
+		attrs []slog.Attr
+	)
+	for len(args) > 0 {
+		attr, args = argsToAttr(args)
+		attrs = append(attrs, attr)
+	}
+	return attrs
+}
+
+func argsToAttr(args []any) (slog.Attr, []any) {
+	switch x := args[0].(type) {
+	case string:
+		if len(args) == 1 {
+			return slog.String(badKey, x), nil
+		}
+		return slog.Any(x, args[1]), args[2:]
+
+	case slog.Attr:
+		return x, args[1:]
+
+	default:
+		return slog.Any(badKey, x), args[1:]
+	}
 }
